23cdf9df03a7108b8150a2c48fa9347683dcf3a4
Cyphio <Harry@mbr-online.co.uk>
2020-04-20 22:00:25 +0100

    Cleaning code

--- core/src/Screens/GameScreen.java
+++ core/src/Screens/GameScreen.java
@@ -28,7 +28,6 @@ import misc.Coordinate;
 
 import java.util.ArrayList;
 
-
 public class GameScreen implements Screen {
 
     private final PropertyTycoon game;
@@ -41,55 +40,34 @@ public class GameScreen implements Screen {
     private Label tileNameLabel;
     private Window tilePopUpMenu;
     private SpriteBatch spriteBatch;
-    Stage overlay;
-
-    OrthographicCamera overlayCam;
-
-
-
-    TiledMap tiledMap;
-    TiledMapRenderer tiledMapRenderer;
+    private Stage overlay;
+    private OrthographicCamera overlayCam;
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
 
     public GameScreen(PropertyTycoon game) {
         this.game = game;
         this.stage = new Stage(new ScreenViewport());
-
-
-
-
 
         Gdx.input.setInputProcessor(stage);
         this.gameScreenTexture = new Texture(Gdx.files.internal("board/board.PNG"));
         this.gameScreenTexture.setWrap(Texture.TextureWrap.ClampToEdge, Texture.TextureWrap.ClampToEdge);
         this.gameScreenSkin = new Skin(Gdx.files.internal("skin/comic-ui.json"));
 
-        System.out.print("HERE");
-
-
-
-
-
         //TILED MAP INITIALIZATION
         tiledMap = new TmxMapLoader().load("core/assets/board/board.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
         layer = (TiledMapTileLayer) tiledMap.getLayers().get("Tile Layer 1");
 
-
         //GAME CONTROLLER
         gameCon = new GameController(layer);
-
-
 
         //TOKEN ADDED TO GO SCREEN
         spriteBatch = new SpriteBatch();
 
         for(Player p : game.players) {
-
             p.getPlayerToken().setPosition(p.getCurrentCoordinates().getX(), p.getCurrentCoordinates().getY());
         }
-
-
-
 
         // POP UP MENU FOR PROPERTIES
         tileNameLabel = new Label("Name", gameScreenSkin);
@@ -106,20 +84,11 @@ public class GameScreen implements Screen {
         stage.addActor(tilePopUpMenu);
 
         closePropMenu.addListener(new ClickListener(){
-
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 tilePopUpMenu.setVisible(false);
             }
         });
-
-
-
-
-
-//        Thread gameThread = new Thread((Runnable) gameCon);
-//        gameThread.start();
-
     }
 
     @Override
@@ -142,7 +111,6 @@ public class GameScreen implements Screen {
         Button pause = new TextButton("Pause", gameScreenSkin);
         Button rollDice = new TextButton("Roll Dice", gameScreenSkin);
 
-
         pause.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -161,78 +129,45 @@ public class GameScreen implements Screen {
             }
         });
 
-
         buttons.row().pad(10, 0, 0, 20);
         buttons.add(pause);
         buttons.row().pad(10, 0, 0, 20);
         buttons.add(rollDice);
-
 
         stage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Vector3 mouse = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
                 camera.unproject(mouse);
-
                 System.out.println(mouse);
-
                 try {
-
                     Tile tile = gameCon.retTile(layer.getCell((((int) mouse.x) / 64), (((int) mouse.y) / 64)));
-
-
                     if (tile instanceof Property){
-
                         tilePopUpMenu.getTitleLabel().setText(tile.getTileName());
                         tilePopUpMenu.getTitleLabel().setColor(Color.BLUE);
                         tilePopUpMenu.setVisible(true);
-
-
-
-
-                    }else if (tile instanceof Jail){
-
-                        System.out.print(tile.getAllCoordinates().get(1).getX());
-
-
-                    }else{
-
-                        tilePopUpMenu.setVisible(false);
-
-
                     }
-
-
-
+                    else if (tile instanceof Jail){
+                        System.out.print(tile.getAllCoordinates().get(1).getX());
+                    }
+                    else{
+                        tilePopUpMenu.setVisible(false);
+                    }
                     if (true) {
-
                         ArrayList<Coordinate> cs = gameCon.retTile(layer.getCell((((int) mouse.x) / 64), (((int) mouse.y) / 64))).getAllCoordinates();
                         System.out.println(cs.size());
                         System.out.println(gameCon.retTile(layer.getCell((((int) mouse.x) / 64), (((int) mouse.y) / 64))));
-
                         for (Coordinate c : cs) {
-
                             layer.getCell(c.getX()/64, c.getY()/64).setTile(null);
-
-                            System.out.print("WHAT");
-
-
-
                         }
                     }
-
-
-                } catch (Exception e) {
+                }
+                catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-
-
             }
         });
-
-
     }
-
 
     @Override
     public void render(float delta) {
@@ -241,33 +176,24 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-
-
         camera.position.set(1120, 1120, 0);
         camera.zoom = (float) 2.5;
 
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
 
-
         spriteBatch.setProjectionMatrix(camera.combined);
-
 
         spriteBatch.begin();
         for(Player p : game.players) {
             p.getPlayerToken().draw(spriteBatch);
-
-
         }
         spriteBatch.end();
 
-
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-
             camera.rotate(90);
             camera.update();
         }
-
 
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
